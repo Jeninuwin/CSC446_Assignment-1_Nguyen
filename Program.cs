7dbe6026a3173b4d7168c0b81bf7376e3b99d820
Jenny Nguyen <44487057+Jeninuwin@users.noreply.github.com>
2022-01-27 23:48:33 -0600

    adding read file and classes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
+
 namespace CSC446_Assignment_1_Nguyen
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string lines = System.IO.File.ReadAllText(@"C:\Users\Daehy\OneDrive - South Dakota State University - SDSU\Desktop\Class\Compliers\test.txt");
+
+            // Display the file contents to the console. Variable text is a string.
+
+            LexAnalyzer(lines);
+
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+
+        static void LexAnalyzer(string text)
+        {
+            Console.WriteLine("This is the LexAnalyzer");
+            Console.WriteLine("Contents of WriteText.txt = {0}", text);
+
+        }
+
+        static void SymbolTable()
+        {
+
         }
     }
 }
