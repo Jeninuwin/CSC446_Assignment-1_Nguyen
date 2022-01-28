ec99acd043dcf65d5b48c5e2df45d86c54f875c9
Jenny Nguyen <44487057+Jeninuwin@users.noreply.github.com>
2022-01-27 21:56:03 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSC446_Assignment_1_Nguyen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
