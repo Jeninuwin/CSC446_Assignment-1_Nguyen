55a18df7fae2bf9ef4751daf1bff95cff5265e0c
Jenny Nguyen <44487057+Jeninuwin@users.noreply.github.com>
2022-01-31 09:58:53 -0600

    updating processwordtoken and getnextchar. Trying to read the next character

--- Program.cs
+++ Program.cs
@@ -1,33 +1,155 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace CSC446_Assignment_1_Nguyen
 {
     class Program
     {
+        static int intgerToken; //interger token 
+        static string literalToken; //literal token 
+        static double realToken; //real token 
+        static string Lexeme;
+        static int line;
+        static string Token;
+        static char temp;
+        static char character;
+        static string[] reservedWords = { "if", "else", "while", "float", "int", "char", "break", "continue", "float", "void" };
+        public enum Symbols
+        {
+            ift, elset, whilet, floatt, intt, chart, breakt, continuet, voidt
+        }
+
+        static string[] addop = { "+", "-", "||" };
+        static string[] mulop = { "*", "/", "%", "&&" };
+        static string[] assignop = { "=" };
+        static string[] symbols = { "(", ")", "{", "}", "[", "]", ",", ";", ".", "\"", };
+        static string[] relop = { "==", "!=", "<", "<=", ">", ">=" };
+
+        static StreamReader readingLines;
+
         static void Main(string[] args)
         {
+            string directory = Directory.GetCurrentDirectory() + "\\";
+            string fileName = "";
 
-            string lines = System.IO.File.ReadAllText(@"C:\Users\Daehy\OneDrive - South Dakota State University - SDSU\Desktop\Class\Compliers\test.txt");
+        filecheck:
+            if (args.Length == 0)
+            {
+                do
+                {
+                    Console.WriteLine("Enter in your c-- file name: ");
+                    fileName = Console.ReadLine();
+                } while (string.IsNullOrWhiteSpace(fileName));
+            }
 
-            // Display the file contents to the console. Variable text is a string.
+            if (args.Length > 0 && File.Exists(directory + args[0]))
+            {
+                fileName = directory + args[0];
+            }
 
-            LexAnalyzer(lines);
+            else if (File.Exists(directory + fileName))
+            {
+                fileName = directory + fileName;
+            }
+
+            else
+            {
+                Console.WriteLine("Error! File was not found!");
+                goto filecheck;
+            }
+
+            LexicalAnalyzer(fileName);
 
             Console.WriteLine("Press any key to exit.");
             System.Console.ReadKey();
         }
 
-        static void LexAnalyzer(string text)
+
+        /*Lexical Analyzer*/
+        static void LexicalAnalyzer(string fileName)
         {
-            Console.WriteLine("This is the LexAnalyzer");
-            Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            readingLines = new StreamReader(fileName);
+            character = (char)readingLines.Read();
+
+            Console.WriteLine("This is the Lexcial Analyzer reading all lines");
+
+            processToken();
+        }
+
+        static void processToken()
+        {
+            Console.WriteLine("This is the processToken");
+
+            Lexeme = character.ToString();
+
+            Console.WriteLine(Lexeme[0]);
+            getNextChar(); // one character lookahead
+
+            if (Lexeme[0] >= 'A' && Lexeme[0] <= 'Z' || Lexeme[0] >= 'a' && Lexeme[0] <= 'z')
+            {
+                processWordToken();
+
+            }
+            else if (Lexeme[0] >= '0' && Lexeme[0] <= '9')
+            {
+                processNumToken();
+
+            }
+            else
+            {
+                processSingleToken();
+
+            }
 
         }
 
-        static void SymbolTable()
+        static void processWordToken()
+        {
+            Console.WriteLine("This is processWordToken");
+
+            if (Lexeme.Length <= 27)
+            {
+                Console.WriteLine("Invalid");
+                Lexeme = "unknown";
+            }
+
+        }
+
+        static void getNextChar()
+        {
+            Console.WriteLine("getting getNextChar");
+            do
+            {
+                temp = (char)readingLines.Read();
+                Console.WriteLine(temp);
+                line++;
+
+            } while (temp != ' ');
+        }
+
+        static void getNextToken()
         {
 
         }
+
+
+        static void processNumToken()
+        {
+            Console.WriteLine("This is processNumToken");
+
+        }
+
+        static void processSingleToken()
+        {
+
+        }
+
+        static void display()
+        {
+
+        }
+
     }
 }
